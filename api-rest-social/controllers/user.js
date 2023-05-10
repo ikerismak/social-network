117c49b0d951d6a75fd28a3144f6c03d4d0df714
ikerismak <ikerismak@gmail.com>
2023-05-09 23:09:31 -0600

    feat: user controller  register and login methods added, token generator is pending for the next commit

--- api-rest-social/controllers/user.js
+++ api-rest-social/controllers/user.js
@@ -8,9 +8,9 @@ const testUser = (req, res) => {
   });
 };
 
-// register
+// user register
 
-const register = (req, res) => {
+const register = async (req, res) => {
   //get data from collection
   let data = req.body;
 
@@ -25,45 +25,104 @@ const register = (req, res) => {
 
   // user duplicate record control
 
-  // User.find({
-  //   $or: [
-  //     { email: newUser.email.toLowerCase() },
-  //     { email: newUser.nick.toLowerCase() },
-  //   ],
-  // }).exec((error, users) => {
-  //   if (error)
-  //     return res.status(500).json({
-  //       status: "error",
-  //       message: "Error trying to consult DB",
-  //     });
+  try {
+    // user duplicate record control
+    const existingUser = await User.findOne({
+      $or: [
+        { email: newUser.email.toLowerCase() },
+        { email: newUser.nick.toLowerCase() },
+      ],
+    });
 
-  //   if (users && users.length >= 1) {
-  //     return res.status(200).send({
-  //       status: "success",
-  //       message: "User already exists",
-  //       data: data,
-  //     });
-  //   }
+    if (existingUser) {
+      return res.status(200).send({
+        status: "success",
+        message: "User already exists",
+      });
+    }
 
-  //   // encrypt password
+    // encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const passhashed = await bcrypt.hash(newUser.password, salt);
 
-  //   bcrypt.hash(newUser.password, 10, (error, passhashed) => {
-  //     console.log(passhashed);
-  //   })
+    newUser.password = passhashed;
 
+    await newUser.save((error, userStored) => {
+      if (error || !userStored)
+        return res.status(500).send({ status: "error" });
+    });
 
-  // });
-
-  return res.status(200).send({
-    status: "success",
-    message: "User registered successfully",
-    newUser: newUser
-  });
+    return res.status(200).send({
+      status: "success",
+      message: "User registered successfully",
+      newUser: newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "error",
+      message: "Error trying to consult DB",
+    });
+  }
 };
 
-//user validate
+const login = async (req, res) => {
+  let userData = req.body;
+
+  if (!userData.email || !userData.password) {
+    return res.status(500).send({
+      status: "error",
+      message: "missing data",
+    });
+  }
+
+  try {
+    const dataForLogin = await User.findOne({ email: userData.email });
+    // .select({ password: 0});
+
+    if (!dataForLogin) {
+      return res.status(404).send({
+        status: "error",
+        message: "user does not exist in DB",
+      });
+    }
+    // check and compare password
+
+    const verifyPassword = bcrypt.compareSync(
+      userData.password,
+      dataForLogin.password
+    );
+
+    if (!verifyPassword) {
+      return res.status(400).send({
+        status: "error",
+        message: "The password is incorrect",
+      });
+    }
+
+    const token = false;
+
+    return res.status(200).send({
+      status: "success",
+      message: "User login successfully",
+      dataForLogin: {
+        id: dataForLogin.id,
+        name: dataForLogin.name,
+        nickname: dataForLogin.nickname,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      status: "error",
+      message: "Error trying to consult DB",
+    });
+  }
+};
 
 module.exports = {
   testUser,
   register,
+  login,
 };
--- api-rest-social/routes/user.js
+++ api-rest-social/routes/user.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const UserController = require("../controllers/user");
 
 
-
-
-
 router.get('/test-user', UserController.testUser);
 router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+
 
 
 module.exports = router;
